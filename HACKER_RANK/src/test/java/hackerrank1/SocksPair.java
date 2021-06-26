b046f0be57c72b21e4c49cd73f516f2311c1bff6
nikhilkanojia <nikhilkanojia@localhost>
2021-06-26 16:51:38 +0530

    commit

---
+++ HACKER_RANK/src/test/java/hackerrank1/SocksPair.java
@@ -0,0 +1,75 @@
+package hackerrank1;
+
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.function.*;
+import java.util.regex.*;
+import java.util.stream.*;
+import static java.util.stream.Collectors.joining;
+import static java.util.stream.Collectors.toList;
+
+class Result {
+
+    /*
+     * Complete the 'sockMerchant' function below.
+     *
+     * The function is expected to return an INTEGER.
+     * The function accepts following parameters:
+     *  1. INTEGER n
+     *  2. INTEGER_ARRAY ar
+     */
+
+    public static int sockMerchant(int n, List<Integer> ar) {
+    	
+    	HashSet<Integer> hs=new HashSet<Integer>();
+    	
+    	int count=0;
+    	
+    	for(int e:ar) {
+    		
+    		if(hs.add(e)==false) {
+    			
+    			hs.remove(e);
+    			count++;
+    			  			
+    		}
+    		
+    		
+    	}
+    	
+    	
+		return count;
+    	
+        
+
+    
+
+}
+}
+
+
+
+public class SocksPair {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter("/Users/nikhilkanojia/seleniumProject/HACKER_RANK/src/test/resources/output.txt"));
+
+        int n = Integer.parseInt(bufferedReader.readLine().trim());
+
+        List<Integer> ar = Stream.of(bufferedReader.readLine().replaceAll("\\s+$", "").split(" "))
+            .map(Integer::parseInt)
+            .collect(toList());
+
+        int result = Result.sockMerchant(n, ar);
+
+        bufferedWriter.write(String.valueOf(result));
+        bufferedWriter.newLine();
+
+        bufferedReader.close();
+        bufferedWriter.close();
+    }
+}
